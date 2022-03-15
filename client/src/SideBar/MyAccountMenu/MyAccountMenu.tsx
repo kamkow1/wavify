a42b49998007935a620288f4a4e2f5599c8195c0
kamkow1 <ikamileq@gmail.com>
2022-03-14 17:38:33 -0700

    sidebar with folded elements

---
+++ client/src/SideBar/MyAccountMenu/MyAccountMenu.tsx
@@ -0,0 +1,11 @@
+import './MyAccountMenu.scss';
+
+export const MyAccountMenu = (): JSX.Element => {
+    return(
+        <ul className="list-group" id="menu-box">
+            <li className="list-group-item header-item">test</li>
+
+            <li className="list-group-item bg-white">wyloguj</li>
+        </ul>
+    );
+}
