746be42cd67e259bc3d57d5d894443b1d35c7b2c
kamkow1 <ikamileq@gmail.com>
2022-03-16 15:28:47 -0700

    changed styles and theming

--- client/src/SideBar/MyAccountMenu/MyAccountMenu.tsx
+++ client/src/SideBar/MyAccountMenu/MyAccountMenu.tsx
@@ -7,7 +7,10 @@ export default class MyAccountMenu extends React.Component<{ usernameToDisplay: 
     render() {
         return(
             <ul className="list-group" id="menu-box">
-                <li className="list-group-item header-item">wyświetl profil { this.props.usernameToDisplay !== undefined && this.props.usernameToDisplay }</li>
+                <li className="list-group-item" id="header-item">
+                    <span className="display-profile">wyświetl profil </span>
+                    <span className="display-username">{ this.props.usernameToDisplay !== undefined && this.props.usernameToDisplay }</span>
+                </li>
     
                 <li className="list-group-item bg-white">wyloguj</li>
             </ul>
